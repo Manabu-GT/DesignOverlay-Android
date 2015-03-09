6984116354b8f87f1b24b6c3a2359c725c2ddfc3
Manabu-GT <manabu1984+github@gmail.com>
2015-03-10 01:58:59 +0900

    remove fabric (crashlytics)

--- app/src/main/java/com/ms_square/android/design/overlay/app/DesignOverlayApplication.java
+++ app/src/main/java/com/ms_square/android/design/overlay/app/DesignOverlayApplication.java
@@ -3,11 +3,7 @@ package com.ms_square.android.design.overlay.app;
 import android.app.Application;
 import android.preference.PreferenceManager;
 
-import com.crashlytics.android.Crashlytics;
-import com.ms_square.android.design.overlay.BuildConfig;
 import com.ms_square.android.design.overlay.R;
-
-import io.fabric.sdk.android.Fabric;
 
 public class DesignOverlayApplication extends Application {
 
@@ -17,9 +13,5 @@ public class DesignOverlayApplication extends Application {
 
         // set default values for the app preference
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
-
-        if (BuildConfig.USE_CRASHLYTICS) {
-            Fabric.with(this, new Crashlytics());
-        }
     }
 }
