6df917a1f1e1cad91d1bc247a72803fb4db8cd86
Manabu-GT <manabu1984+github@gmail.com>
2015-03-21 03:26:05 +0900

    enable timber for debug

--- app/src/main/java/com/ms_square/android/design/overlay/app/DesignOverlayApplication.java
+++ app/src/main/java/com/ms_square/android/design/overlay/app/DesignOverlayApplication.java
@@ -3,13 +3,21 @@ package com.ms_square.android.design.overlay.app;
 import android.app.Application;
 import android.preference.PreferenceManager;
 
+import com.ms_square.android.design.overlay.BuildConfig;
 import com.ms_square.android.design.overlay.R;
+
+import timber.log.Timber;
 
 public class DesignOverlayApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            // set up Timber for logging
+            Timber.plant(new Timber.DebugTree());
+        }
 
         // set default values for the app preference
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
